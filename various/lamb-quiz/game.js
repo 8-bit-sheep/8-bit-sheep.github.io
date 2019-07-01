ceeaa213997f95c78161a1b388c902a66093f62a
skallinen <notjustsilicon@gmail.com>
2019-07-01 20:18:09 +0300

    latest additions

--- various/lamb-quiz/game.js
+++ various/lamb-quiz/game.js
@@ -9,8 +9,9 @@ const scoreText = document.getElementById("score");
 const episodeNameText = document.getElementById("episodeName");
 const contentNameText = document.getElementById("contentName");
 const urlNameText = document.getElementById("episodeurl");
-const id =  window.location.search.split("=")[1];
-
+const id = window.location.search.split("=")[1];
+const segmentBox = document.getElementById("segment-index");
+const segmentButtons = document.getElementById("segment-buttons");
 
 // state
 let currentQuestion = {};
@@ -23,79 +24,145 @@ let questions = [];
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 50;
 
-
 const startGame = () => {
-    questionCounter = 0;
-    score = 0;
+  questionCounter = 0;
+  score = 0;
+  loader.classList.add("hidden");
+  if (id === "0") {
+    game.classList.remove("hidden");
     availableQuestions = [...questions];
     getNewQuestion();
-    game.classList.remove("hidden");
-    loader.classList.add("hidden");
-}
+  } else {
+    segmentBox.classList.remove("hidden");
+    segmentSelector();
+  }
+};
+
+const segmentSelector = () => {
+  const segmentList = [
+    ...new Set(questions.map(question => question.contentSegment))
+  ];
+  segmentList.forEach(segment =>
+    createButtonInsideListItem(segmentButtons, segment)
+  );
+};
+
+const createButtonInsideListItem = (list, text) => {
+  const li = document.createElement("li");
+  list.appendChild(li);
+  const button = document.createElement("button");
+  li.appendChild(button);
+  button.innerText = text;
+  button.classList.add("segment-btn");
+  button.addEventListener("click", startSegmentGame);
+};
+
+const startSegmentGame = e => {
+  availableQuestions = questions.filter(
+    question => question.contentSegment === e.target.innerText
+  );
+  getNewQuestion();
+  game.classList.remove("hidden");
+  segmentBox.classList.add("hidden");
+};
 
 const getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS - 1){
-        localStorage.setItem("mostRecentScore", score);
-        return window.location.assign("end.html?contentId=" + id);
-    }
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS - 1) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("end.html?contentId=" + id);
+  }
 
-    questionCounter++;
-    questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    episodeNameText.innerText = currentQuestion.contentSegment;
-    contentNameText.innerText = currentQuestion.content;
-    question.innerText = currentQuestion.question;
-    urlNameText.href = currentQuestion.url;
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
-    })
-    
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+  questionCounter++;
+  questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  episodeNameText.innerText = currentQuestion.contentSegment;
+  contentNameText.innerText = currentQuestion.content;
+  question.innerText = currentQuestion.question;
+  urlNameText.href = currentQuestion.url;
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-}
-
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
-        acceptinganswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-        const classToApply = (selectedAnswer === currentQuestion.answer)? "correct":"incorrect";
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+    acceptinganswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    const classToApply =
+      selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
 
-        if(classToApply === "correct") {
-            incrementScore(CORRECT_BONUS);
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    if (classToApply === "correct") {
+      setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+      }, 500);
+    } else {
+      wrongAnswer();
+    }
+    /*else {
+      //setTimeout(() => {
+      choices.forEach(choice => {
+        if (choice.dataset["number"] === currentQuestion.answer) {
+          choice.parentElement.classList.add("correct");
         }
-        
-        console.log(classToApply);
-        console.log(selectedAnswer);
+      });
+      //}, 200);
+      //acceptingAnswers = false;
+      console.log(acceptingAnswers);
+      //setTimeout(() => {
+      addEventListener("click", e => {
+        choices.forEach(choice => {
+          choice.parentElement.classList.remove(["incorrect"]);
+          choice.parentElement.classList.remove(["correct"]);
+        });
+        acceptingAnswers = false;
+        getNewQuestion();
+      });
+      //}, 500);
+    }*/
+  });
+});
 
-        selectedChoice.parentElement.classList.add(classToApply);
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 500);
-    })
-})
+const wrongAnswer = () => {
+  setTimeout(() => {
+    choices.forEach(choice => {
+      if (choice.dataset["number"] === currentQuestion.answer) {
+        choice.parentElement.classList.add("correct");
+      }
+    });
+  }, 200);
+  setTimeout(() => {
+    choices.forEach(choice => {
+      choice.parentElement.classList.remove(["incorrect"]);
+      choice.parentElement.classList.remove(["correct"]);
+    });
+    getNewQuestion();
+  }, 1500);
+};
 
 const incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
-}
+  score += num;
+  scoreText.innerText = score;
+};
 
-
-
-d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSfBk-rwrIauBPn7iuoLXBxP2sSYOXRYCbJ2GflzSK6wxGVGDr_fAqORJ0JWPdajFLxnGegmrlI26HB/pub?output=csv")
-    .then((data) => {
-        questions = data.filter(question => {
-            if(id === "0") return true
-            else return question.contentId === id;
-        });
-        startGame();
-    })
-    .catch(function(error){
-        //
-    })
+d3.csv(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfBk-rwrIauBPn7iuoLXBxP2sSYOXRYCbJ2GflzSK6wxGVGDr_fAqORJ0JWPdajFLxnGegmrlI26HB/pub?output=csv"
+).then(data => {
+  questions = data.filter(question => {
+    if (id === "0") return true;
+    else return question.contentId === id;
+  });
+  startGame();
+});
