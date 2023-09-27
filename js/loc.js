30786437baa736cb4bc249843abcd9e43083dd7f
Sami Kallinen <notjustsilicon@gmail.com>
2023-09-27 20:37:40 +0300

    adding dynamic location strip. edit your location on a drive spreadsheet...

---
+++ js/loc.js
@@ -0,0 +1,52 @@
+let csvUrl = "https://europe-west1-csv-proxy.cloudfunctions.net/csv";
+
+const fetchDataAndRender = async () => {
+    try {
+        const response = await fetch(csvUrl);
+        const data = await response.text();
+        const records = data.split("\n").slice(1);
+        const currentDate = new Date();
+        let validCities = records.reduce((accumulator, record) => {
+            const [place, sheep, from, until] = record.split(",");
+            const fromDate = from ? new Date(from) : null;
+            const untilDate = until ? new Date(until) : null;
+
+            if (!accumulator[sheep] || (fromDate && fromDate <= currentDate
+                                        && (!untilDate || untilDate >= currentDate)
+                                        && (!accumulator[sheep].fromDate || fromDate > accumulator[sheep].fromDate))) {
+                accumulator[sheep] = {
+                    place,
+                    fromDate,
+                    untilDate
+                };
+            }
+            return accumulator;
+        }, {});
+
+        let seenCities = new Set();
+        validCities = Object.values(validCities)
+            .map(record => record.place.trim())
+            .filter((city) => {
+                const lowerCaseCity = city.toLowerCase();
+                if (!seenCities.has(lowerCaseCity)) {
+                    seenCities.add(lowerCaseCity);
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+            .sort();
+
+        document.querySelector("#location").innerText = validCities.join(' | ');
+    } catch (error) {
+        console.error('Fetch data error: ', error);
+    }
+};
+
+
+
+// Initial Call
+fetchDataAndRender();
+
+// Fetches the csv every 4 hours and re-renders the locations element 
+setInterval(fetchDataAndRender, 1000*60*60*1);
