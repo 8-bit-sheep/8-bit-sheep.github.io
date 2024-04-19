644edf05896006d959b0a7ea58d26be9f2291f54
gronerik <118974043+gronerik@users.noreply.github.com>
2024-04-19 13:51:34 +0300

    Update utils.js, remove unused References section

--- js/utils.js
+++ js/utils.js
@@ -85,43 +85,6 @@ window.onload = function(){
 		.typeString(charInfinite)
 		.start();
 
-	/* References */
-	// Init vars
-	/*
-	var referenceNavItems = document.querySelectorAll(".reference-nav a");
-	var referenceCases = document.querySelectorAll(".reference-case");
-	
-	// Loop thru items
-	referenceNavItems.forEach((referenceNavItem, i) => {
-	
-		// Store ref case index
-		var referenceCase = referenceCases[i];
-		
-		// Event listeners
-		referenceNavItem.addEventListener("click", function(e){
-		
-			// Prevent href default behavior
-			e.preventDefault();
-			
-			// Hide all - Nav items
-			referenceNavItems.forEach((navItems) => {
-				navItems.className = ""; // Clear class
-			});
-			
-			// Hide all - Cases
-			referenceCases.forEach((cases) => {
-				cases.className = "reference-case"; // Set class to default
-			});
-			
-			// Show active - Nav item + case
-			referenceNavItem.className += "active";
-			referenceCase.className += "active";
-		
-		});
-		
-	});
-	*/
-
 	/* Location */
 	let resultDiv = document.querySelector("#location");
 	let csvUrl = "https://europe-west1-csv-proxy.cloudfunctions.net/csv";
